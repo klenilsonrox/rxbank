ae827e342129efa3fa950104b3b59590c18ccfca
klenilsonrox <klenilsoncoc@gmail.com>
2024-07-17 13:19:42 -0300

    a

--- src/app/components/Footer.js
+++ src/app/components/Footer.js
@@ -7,11 +7,11 @@ const Footer = () => {
       <footer className="max-w-lg w-full mx-auto p-6">
 
             <ul className="text-white flex w-full max-w-3xl justify-between mx-auto">
-              <li><a href="/politica" className="hover:underline">Política</a></li>
-              <li><a href="/contato" className="hover:underline">Contato</a></li>
-              <li><a href="/termos" className="hover:underline">Termos</a></li>
-              <li><a href="/inicio" className="hover:underline">Início</a></li>
-              <li><a href="/sobre" className="hover:underline">Sobre</a></li>
+              <li><a href="/" className="hover:underline">Política</a></li>
+              <li><a href="/" className="hover:underline">Contato</a></li>
+              <li><a href="/" className="hover:underline">Termos</a></li>
+              <li><a href="/" className="hover:underline">Início</a></li>
+              <li><a href="/" className="hover:underline">Sobre</a></li>
             </ul>
   
         <div className="mt-6 flex justify-center space-x-4">
